74127768543252d690ac5bafdb15317c5e0ad54a
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-10-04 20:44:14 +1300

    merged top pick and category again

--- app/src/main/java/com/softeng306/p2/MainActivity.java
+++ app/src/main/java/com/softeng306/p2/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SearchView;
 import android.os.Bundle;
 
 import com.softeng306.p2.Adapter.TopAdapter;
@@ -14,6 +19,11 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity {
+    class ViewHolder {
+        private CardView CatElectric, CatHybrid, CatPetrol;
+        private SearchView SearchBar;
+    }
+
     //Initialize variable
     RecyclerView recyclerView;
 
@@ -50,5 +60,39 @@ public class MainActivity extends AppCompatActivity {
         //Initialize top adapter
         topAdapter = new TopAdapter(MainActivity.this,topModels);
         recyclerView.setAdapter(topAdapter);
+
+        // initialise views for future references
+        ViewHolder vh = new ViewHolder();
+        vh.SearchBar = (SearchView) findViewById(R.id.SearchBar);
+        vh.CatElectric = (CardView) findViewById(R.id.CatElectric);
+        vh.CatHybrid = (CardView) findViewById(R.id.CatHybrid);
+        vh.CatPetrol = (CardView) findViewById(R.id.CatPetrol);
+
+        // Set up the search bar
+        vh.SearchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String searchInput) {
+                SearchEventHandler(searchInput);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String searchInput) {
+                return false;
+            }
+        });
+    }
+
+    // Open search activity with results of the phrase inputted by user
+    public void SearchEventHandler(String phrase) {
+        Log.i("MainActivity", "Searching for " + phrase);
+        /* TO DO */
+    }
+
+    // Open list activity based on the category clicked on
+    public void CategoryEvenHandler(View v) {
+        CardView category = (CardView) v;
+        Log.i("MainActivity", "Opening " + category.getContentDescription());
+        /* TO DO */
     }
 }

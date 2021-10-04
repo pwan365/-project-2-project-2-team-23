b753e4e8745e21ec593bb8eda483f7777d5be43b
pwan365 <pwan365@aucklanduni.ac.nz>
2021-10-04 17:35:48 +1300

    children classes of Vehicle

---
+++ app/src/main/java/com/softeng306/p2/Models/Hybrid.java
@@ -0,0 +1,26 @@
+package com.softeng306.p2.Models;
+
+public class Hybrid extends Vehicle{
+    private boolean isPHEV;
+    private int chargingTime;
+
+    public Hybrid(int id, String name) {
+        super(id, name);
+    }
+
+    public boolean getIsPHEV(){
+        return isPHEV;
+    }
+
+    public void setIsPHEV(boolean b){
+        isPHEV = b;
+    }
+
+    public int getChargingTime(){
+        return chargingTime;
+    }
+
+    public void setChargingTime(int i){
+        chargingTime = i;
+    }
+}

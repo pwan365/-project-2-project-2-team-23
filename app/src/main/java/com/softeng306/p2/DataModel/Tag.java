bb97a60e698225cf19009b5e353d7d548ab87ce7
pwan365 <pwan365@aucklanduni.ac.nz>
2021-10-16 13:38:17 +1300

    updated tag class

--- app/src/main/java/com/softeng306/p2/DataModel/Tag.java
+++ app/src/main/java/com/softeng306/p2/DataModel/Tag.java
@@ -1,15 +1,21 @@
 package com.softeng306.p2.DataModel;
 
 public class Tag {
+    private int id;
     private String tagName;
     private String tagType;
 
     public Tag(int newId, String newTagName, String newTagType){
+        id = newId;
         tagName = newTagName;
         tagType = newTagType;
     }
 
     public Tag(){}
+
+    public int getId(){
+        return id;
+    }
 
     public String getTagName(){
         return tagName;
